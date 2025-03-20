a78edce8ce6bc749b1c501be97da7ec099d5a301
kyleehudran <KMhudran@student.polandschools.org>
2025-03-20 10:16:51 -0400

    Add files via upload

---
+++ Food _list.py
@@ -0,0 +1,9 @@
+prompt = "\nEnter your favorite foods to buy."
+prompt += "\nEnter 'quit' when you are finished. "
+
+while True:
+    foods = eggs, milk and goldfish(buying)
+    if food == "quit":
+        break 
+    else:
+        print(f"My favortie foods to buy at the store{eggs,milk,and goldfish}!")
